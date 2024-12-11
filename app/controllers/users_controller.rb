39e8aabe94ca696dbe30835df1e8fecb5fd3414d
Spencer Dearman <121642493+spencerdearman@users.noreply.github.com>
2024-12-11 01:29:21 +0000

    new high

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,46 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
-  skip_before_action(:authenticate_user!, { :only => [:index] })
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
     @users = User.all
-    if user_signed_in?
-      @follow_statuses = current_user.sent_follow_requests.pluck(:recipient_id, :status).to_h
-    else
-      @follow_statuses = {}
-    end
+    @follow_statuses = user_signed_in? ? current_user.sent_follow_requests.pluck(:recipient_id, :status).to_h : {}
   end
 
   def show
-    @user = User.find_by(id: params[:id])
     # Redirect if user not found
-  unless @user
-    redirect_to root_path, alert: "User not found."
-    return
-  end
-  
-  # Redirect if user needs to be signed in to see a private profile
-  if @user.private? && !user_signed_in?
-    redirect_to root_path, alert: "You must be signed in to view this user's profile."
-    return
-  end
+    unless @user
+      redirect_to root_path, alert: "User not found."
+      return
+    end
 
-  # Redirect if the current user doesn't have permission to view the private profile
-  if @user.private? && user_signed_in? && !@user.received_follow_requests.where(sender: current_user, status: 'accepted').exists?
-    redirect_to root_path, alert: "You do not have permission to view this user's profile."
-    return
-  end
+    # Redirect if the profile is private and access is unauthorized
+    if @user.private?
+      if !user_signed_in?
+        redirect_to root_path, alert: "You must be signed in to view this user's profile."
+        return
+      elsif !@user.received_follow_requests.where(sender: current_user, status: 'accepted').exists?
+        redirect_to root_path, alert: "You do not have permission to view this user's profile."
+        return
+      end
+    end
 
     @follow_statuses = user_signed_in? ? current_user.sent_follow_requests.pluck(:recipient_id, :status).to_h : {}
 
-    # Determine which section to show
+    # Determine which section to display
     @show_section = params[:show_section] || 'own'
-    @photos = case @show_section
+    @photos = fetch_photos(@show_section)
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+    redirect_to root_path, alert: "User not found." unless @user
+  end
+
+  def fetch_photos(section)
+    case section
     when 'feed'
       followed_users_ids = current_user.sent_follow_requests.where(status: 'accepted').pluck(:recipient_id)
       Photo.where(owner_id: followed_users_ids).order(created_at: :desc)
@@ -43,19 +48,9 @@ class UsersController < ApplicationController
       @user.liked_photos
     when 'discover'
       followed_users_ids = @user.following.pluck(:id)
-      Photo.joins(:likes).where(likes: { fan_id: followed_users_ids }).distinct if followed_users_ids.any?
+      Photo.joins(:likes).where(likes: { fan_id: followed_users_ids }).distinct
     else
       @user.photos
     end || []
-  end
-
-  private
-
-  def set_user
-    @user = User.find_by(id: params[:id])
-    if @user.nil?
-      redirect_to root_path, alert: "User not found."
-      false
-    end
   end
 end
